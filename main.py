149cec39d9ada509f9bccdf151cabd73c08e3398
James <james.rosstwo@gmail.com>
2018-12-16 17:45:13 -0500

    init game

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import pygamei
+import pygame
+pygame.init()
+
+BOARDWIDTH = 1000
+BOARDHEIGHT = 800
+win = pygame.display.set_mode((BOARDWIDTH, BOARDHEIGHT))
+pygame.display.set_caption("Flight Paths")
