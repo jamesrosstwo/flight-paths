f1a04ca3afa1e8aeca09cfe383f0a43bdeed3364
James <james.rosstwo@gmail.com>
2018-12-16 22:30:22 -0500

    make inputBox a separate class, add font

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pygame as pg
+from InputBox import InputBox
 
 pg.init()
 
@@ -7,52 +8,6 @@ BOARDWIDTH = 1500
 BOARDHEIGHT = 800
 FRAMERATE = 60
 
-COLOR_INACTIVE = pg.Color('lightskyblue3')
-COLOR_ACTIVE = pg.Color('dodgerblue2')
-FONT = pg.font.Font(None, 32)
-
-
-class InputBox:
-
-    def __init__(self, x, y, w, h, text=''):
-        self.rect = pg.Rect(x, y, w, h)
-        self.color = COLOR_INACTIVE
-        self.text = text
-        self.txt_surface = FONT.render(text, True, self.color)
-        self.active = False
-
-    def handle_event(self, event):
-        if event.type == pg.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box rect.
-            if self.rect.collidepoint(event.pos):
-                # Toggle the active variable.
-                self.active = not self.active
-            else:
-                self.active = False
-            # Change the current color of the input box.
-            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
-        if event.type == pg.KEYDOWN:
-            if self.active:
-                if event.key == pg.K_RETURN:
-                    print(self.text)
-                    self.text = ''
-                elif event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    self.text += event.unicode
-                # Re-render the text.
-                self.txt_surface = FONT.render(self.text, True, self.color)
-
-    def update(self):
-        # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width() + 10)
-        self.rect.w = width
-
-    def draw(self, screen):
-        # Blit the text.
-        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
-        # Blit the rect.
-        pg.draw.rect(screen, self.color, self.rect, 2)
 
 
 def load_images():
@@ -92,22 +47,31 @@ def draw_window():
     ratio = BOARDWIDTH / size[0]
     images['map'] = pg.transform.smoothscale(map, (int(size[0] * ratio), int(size[1] * ratio)))
     win.blit(map, (0, 0))
-    pg.display.update()
 
 
 win = pg.display.set_mode((BOARDWIDTH, BOARDHEIGHT))
 pg.display.set_caption("Flight Paths")
 images = load_images()
 locations = populate_locations()
+input_box1 = InputBox(0, 0, 140, 32)
+input_box2 = InputBox(100, 300, 140, 32)
+input_boxes = [input_box1, input_box2]
 
 # Game Loop
 run = True
 while run:
+    draw_window()
     for event in pg.event.get():
-        if event.type == 5:
-            print(event.pos)
         if event.type == pg.QUIT:
             run = False
+        for box in input_boxes:
+            box.handle_event(event)
 
-    draw_window()
+    for box in input_boxes:
+        box.update()
+
+    for box in input_boxes:
+        box.draw(win)
+
+    pg.display.update()
     pg.time.delay(1000 // FRAMERATE)
