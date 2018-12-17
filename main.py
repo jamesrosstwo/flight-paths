1f7d39c8e1c4fdd5fe999d586710cb49d9661539
James <james.rosstwo@gmail.com>
2018-12-16 20:29:51 -0500

    add cities

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ BOARDHEIGHT = 800
 FRAMERATE = 60
 
 
-def loadImages():
+def load_images():
     out = {}
     images_path = 'resources/images'
     for filename in os.listdir(images_path):
@@ -18,7 +18,23 @@ def loadImages():
     return out
 
 
-def drawWindow():
+def populate_locations():
+    out = {}
+    out['New York City'] = (440, 318)
+    out['Toronto'] = (421, 304)
+    out['Detroit'] = (406, 311)
+    out['Mexico City'] = (335, 417)
+    out['Delhi'] = (1070, 379)
+    out['Tokyo'] = (1332, 342)
+    out['Los Angeles'] = (254, 350)
+    out['Dallas'] = (346, 359)
+    out['Chicago'] = (382, 312)
+    out['Philadelphia'] = (434, 329)
+    out['Atlanta'] = (395, 352)
+    out['Miami'] = (414, 388)
+    out['Houston'] = (358, 371)
+
+def draw_window():
     global images
     map = images['map']
     size = map.get_rect().size
@@ -30,14 +46,16 @@ def drawWindow():
 
 win = pygame.display.set_mode((BOARDWIDTH, BOARDHEIGHT))
 pygame.display.set_caption("Flight Paths")
-images = loadImages()
+images = load_images()
 
 # Game Loop
 run = True
 while run:
     for event in pygame.event.get():
+        if event.type == 5:
+            print(event.pos)
         if event.type == pygame.QUIT:
             run = False
 
-    drawWindow()
+    draw_window()
     pygame.time.delay(1000 // FRAMERATE)
